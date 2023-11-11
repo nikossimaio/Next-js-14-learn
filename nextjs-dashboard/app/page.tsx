387954d0d25c35175b73dbba8369cd38d6a42550
nikossimaio <nikossimaio@gmail.com>
2023-11-11 10:39:18 +0200

    Chapter 4, layout

--- nextjs-dashboard/app/page.tsx
+++ nextjs-dashboard/app/page.tsx
@@ -1,12 +1,14 @@
-import AcmeLogo from '@/app/ui/acme-logo';
-import { ArrowRightIcon } from '@heroicons/react/24/outline';
+import Link from 'next/link';
 import Image from 'next/image';
+import { ArrowRightIcon } from '@heroicons/react/24/outline';
 
+/* Resources */
 import { lusitana } from '@/app/ui/fonts';
+import AcmeLogo from '@/app/ui/acme-logo';
 
+/* CSS */
 import styles from '@/app/ui/home.module.css';
 
-import Link from 'next/link';
 export default function Page() {
   return (
     <main className="flex min-h-screen flex-col p-6">
